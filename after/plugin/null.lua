2cc70ef4a639b5a203badea803cd6241b2c89bec
Fairbrook <kevinvr@hotmail.es>
2023-04-24 17:56:28 -0600

    Fixed next and prev diagnostics

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -5,7 +5,7 @@ null_ls.setup({
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.formatting.eslint_d,
-        null_ls.builtins.formatting.prettier,
+        --null_ls.builtins.formatting.prettier,
         --null_ls.builtins.formatting.prettier_eslint,
         null_ls.builtins.formatting.prismaFmt
     },
