de2d9b66bdc92865e0da4f89dae08e39d64cfe6a
Kevin Alan Martinez Virgen <kevinvr@hotmail.es>
2023-05-12 19:35:47 -0600

    Update remap.lua

--- lua/kevinm/remap.lua
+++ lua/kevinm/remap.lua
@@ -8,6 +8,9 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("v", "<leader>Y", "\"+Y")
+
 vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "<leader>d", "\"_d")
 
