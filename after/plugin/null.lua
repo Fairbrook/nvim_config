d4c6919b91b7532e54667ca01bb902ce0f854c71
Fairbrook <kevinvr@hotmail.es>
2023-06-28 22:02:53 -0600

    added xmlformat

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -2,18 +2,14 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        --null_ls.builtins.diagnostics.eslint,
-        --null_ls.builtins.code_actions.eslint_d,
-        --null_ls.builtins.formatting.eslint_d,
-        --null_ls.builtins.formatting.prettier,
-        --null_ls.builtins.formatting.prettier_eslint,
+        null_ls.builtins.formatting.xmlformat,
         null_ls.builtins.formatting.trim_whitespace,
         null_ls.builtins.formatting.prismaFmt,
---        null_ls.builtins.formatting.prettierd.with({
---            condition = function(utils)
---              return utils.has_file({ ".prettierrc.js" }) or utils.has_file({ ".prettierrc" })
---            end,
---        }),
+        null_ls.builtins.formatting.prettierd.with({
+            condition = function(utils)
+              return utils.has_file({ ".prettierrc.js" }) or utils.has_file({ ".prettierrc" })
+            end,
+        }),
     },
 })
 
