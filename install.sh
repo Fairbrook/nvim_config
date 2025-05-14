f1402d152b296fc3cf1a4340f9ec15484831fe1a
Kevin Alan Martinez Virgen <kevinvr@hotmail.es>
2025-05-13 20:43:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 npm install -g eslint_d @fsouza/prettierd
+cp -r . ~/.config/nvim
 # clone
 git clone https://github.com/powerline/fonts.git --depth=1
 # install
